cb15cee5ac3737e43016130d67abf9443483ea3b
rafaeltondin <149697671+rafaeltondin@users.noreply.github.com>
2024-09-24 09:57:34 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,64 +1,82 @@
 // src/index.js
-
-import { makeWASocket, fetchLatestBaileysVersion, useMultiFileAuthState, DisconnectReason } from '@whiskeysockets/baileys';
+import { makeWASocket, fetchLatestBaileysVersion, DisconnectReason } from '@whiskeysockets/baileys';
 import utils from './utils/utils.js';
 import fs from 'fs';
 import dotenv from 'dotenv';
+import MySQLAuthState from './utils/MySQLAuth.js';
 
 dotenv.config();
 
 async function startWhatsAppSocket() {
-    try {
-        const { state, saveCreds } = await useMultiFileAuthState('./auth_info');
-        const { version } = await fetchLatestBaileysVersion();
+  try {
+    const dbConfig = {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    };
 
-        const sock = makeWASocket({
-            printQRInTerminal: true,
-            auth: state,
-            version,
-        });
+    const authState = new MySQLAuthState(dbConfig);
+    await authState.init();
 
-        sock.ev.on('connection.update', async (update) => {
-            const { connection, lastDisconnect } = update;
-            if (connection === 'close') {
-                if (lastDisconnect && 'error' in lastDisconnect && lastDisconnect.error) {
-                    const boomError = lastDisconnect.error;
-                    const shouldReconnect = boomError.output ? boomError.output.statusCode !== DisconnectReason.loggedOut : true;
-                    console.log('Connection closed due to', boomError, ', reconnecting:', shouldReconnect);
-                    if (shouldReconnect) {
-                        startWhatsAppSocket(); // Reinicia a conexão
-                    } else {
-                        console.log('Desconectado permanentemente');
-                    }
-                }
-            } else if (connection === 'open') {
-                console.log('Conexão aberta com sucesso');
-            }
-        });
+    const { state, saveCreds } = {
+      state: authState.stateData,
+      saveCreds: async () => {
+        await authState.saveState();
+      }
+    };
 
-        sock.ev.on('messages.upsert', async ({ messages }) => {
-            const message = messages[0];
-            console.log(JSON.stringify(message, undefined, 2));
-            if (!message.key.fromMe) {
-                console.log('Respondendo para', message.key.remoteJid);
-                await utils.handleMessage(sock, message);
-            }
-        });
+    const { version } = await fetchLatestBaileysVersion();
 
-        sock.ev.on('creds.update', saveCreds);
+    const sock = makeWASocket({
+      printQRInTerminal: true,
+      auth: state,
+      version,
+    });
 
-        // Criar as pastas necessárias se não existirem
-        const folders = ['audio', 'video', 'images'];
-        for (const folder of folders) {
-            if (!fs.existsSync(folder)) {
-                fs.mkdirSync(folder);
-            }
+    sock.ev.on('connection.update', async (update) => {
+      const { connection, lastDisconnect } = update;
+      if (connection === 'close') {
+        if (lastDisconnect && 'error' in lastDisconnect && lastDisconnect.error) {
+          const boomError = lastDisconnect.error;
+          const shouldReconnect = boomError.output ? boomError.output.statusCode !== DisconnectReason.loggedOut : true;
+          console.log('Connection closed due to', boomError, ', reconnecting:', shouldReconnect);
+          if (shouldReconnect) {
+            startWhatsAppSocket(); // Reinicia a conexão
+          } else {
+            console.log('Desconectado permanentemente');
+          }
         }
-    } catch (error) {
-        console.error('Falha ao iniciar o socket do WhatsApp:', error);
-        // Tente reiniciar a conexão após um erro
-        setTimeout(startWhatsAppSocket, 5000); // Tente reconectar após 5 segundos
+      } else if (connection === 'open') {
+        console.log('Conexão aberta com sucesso');
+        // Salva as credenciais após a conexão
+        await saveCreds();
+      }
+    });
+
+    sock.ev.on('messages.upsert', async ({ messages }) => {
+      const message = messages[0];
+      console.log(JSON.stringify(message, undefined, 2));
+      if (!message.key.fromMe) {
+        console.log('Respondendo para', message.key.remoteJid);
+        await utils.handleMessage(sock, message);
+      }
+    });
+
+    sock.ev.on('creds.update', saveCreds);
+
+    // Criar as pastas necessárias se não existirem
+    const folders = ['audio', 'video', 'images'];
+    for (const folder of folders) {
+      if (!fs.existsSync(folder)) {
+        fs.mkdirSync(folder);
+      }
     }
+  } catch (error) {
+    console.error('Falha ao iniciar o socket do WhatsApp:', error);
+    // Tente reiniciar a conexão após um erro
+    setTimeout(startWhatsAppSocket, 5000); // Tente reconectar após 5 segundos
+  }
 }
 
 startWhatsAppSocket();
